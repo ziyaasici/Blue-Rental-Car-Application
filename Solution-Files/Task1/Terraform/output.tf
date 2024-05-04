8925b3f0df8a74284568dbd57d35e94096e3e4b1
Ziya Asici <ziya.asici@yahoo.com>
2024-05-04 02:26:17 +0000

    Multi-Stage Build Try

--- Solution-Files/Task1/Terraform/output.tf
+++ Solution-Files/Task1/Terraform/output.tf
@@ -1,8 +1,3 @@
-# output "Environment" {
-#   value = "${terraform.workspace}"
-# }
-
 output "ec2-public-ips" {
-   # value = "${terraform.workspace}-${aws_instance.ec2[*]}-ip: ${aws_instance.ec2[*].public_ip}"
    value = aws_instance.ec2[*].public_ip
 }
