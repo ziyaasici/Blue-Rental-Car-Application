301c0b1d411cab81925f2cd46e20a7679fbb3f6b
Ziya Asici <ziya.asici@yahoo.com>
2024-05-09 03:19:38 +0000

    Test

--- Solution-Files/Task4/kompose-convert.sh
+++ Solution-Files/Task4/kompose-convert.sh
@@ -9,18 +9,18 @@ for file in *.yaml; do
     # Set NodePort for UI service
     if grep -q "io.kompose.service: ui" "$file"; then
         # Add the NodePort type correctly
-        sed -i "/ports:/i \ \ type: NodePort" "$file"
-        # Add the nodePort value under the correct port
-        sed -i "/- name: \"3000\"/a \ \ \ \ \ nodePort: $ui_nodeport" "$file"
+        sed -i "/ports:/a \ \ type: NodePort" "$file"
+        # Add the nodePort value under the correct port with the correct indentation
+        sed -i "/- name: \"3000\"/a \ \ \ \ nodePort: $ui_nodeport" "$file"
         ((ui_nodeport++))  # Increment after modifications are done
     fi
     
     # Set NodePort for app service
     if grep -q "io.kompose.service: app" "$file"; then
         # Add the NodePort type correctly
-        sed -i "/ports:/i \ \ type: NodePort" "$file"
-        # Add the nodePort value under the correct port
-        sed -i "/- name: \"8080\"/a \ \ \ \ \ nodePort: $app_nodeport" "$file"
+        sed -i "/ports:/a \ \ type: NodePort" "$file"
+        # Add the nodePort value under the correct port with the correct indentation
+        sed -i "/- name: \"8080\"/a \ \ \ \ nodePort: $app_nodeport" "$file"
         ((app_nodeport++))  # Increment after modifications are done
     fi
 done
