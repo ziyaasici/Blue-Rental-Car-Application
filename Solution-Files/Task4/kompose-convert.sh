6f56fcc1d03dfa9655f1a064a153c6b6430e3c82
Ziya Asici <ziya.asici@yahoo.com>
2024-05-09 02:35:05 +0000

    Script Update

--- Solution-Files/Task4/kompose-convert.sh
+++ Solution-Files/Task4/kompose-convert.sh
@@ -7,14 +7,20 @@ app_nodeport=32000
 # Loop through each generated Kubernetes manifest file
 for file in *.yaml; do
     # Set NodePort for UI service
-    if grep -q "ui:" "$file"; then
-        sed -i "/name: ui/,/type: ClusterIP/ s/type: ClusterIP/type: NodePort\n      nodePort: $ui_nodeport/" "$file"
-        ((ui_nodeport++))
+    if grep -q "io.kompose.service: ui" "$file"; then
+        # Add the NodePort type correctly
+        sed -i "/ports:/i \ \ type: NodePort" "$file"
+        # Add the nodePort value under the correct port
+        sed -i "/- name: \"3000\"/a \ \ \ \ nodePort: $ui_nodeport" "$file"
+        ((ui_nodeport++))  # Increment after modifications are done
     fi
     
     # Set NodePort for app service
-    if grep -q "app:" "$file"; then
-        sed -i "/name: app/,/type: ClusterIP/ s/type: ClusterIP/type: NodePort\n      nodePort: $app_nodeport/" "$file"
-        ((app_nodeport++))
+    if grep -q "io.kompose.service: app" "$file"; then
+        # Add the NodePort type correctly
+        sed -i "/ports:/i \ \ type: NodePort" "$file"
+        # Add the nodePort value under the correct port
+        sed -i "/- name: \"8080\"/a \ \ \ \ nodePort: $app_nodeport" "$file"
+        ((app_nodeport++))  # Increment after modifications are done
     fi
 done
