5468d3c92a63f47e0606069d665be5cff08d642b
Ziya Asici <ziya.asici@yahoo.com>
2024-05-09 00:22:32 +0000

    Jenkins Flow

--- Solution-Files/Task4/kompose-convert.sh
+++ Solution-Files/Task4/kompose-convert.sh
@@ -4,9 +4,6 @@
 ui_nodeport=31000
 app_nodeport=32000
 
-# Convert Docker Compose file to Kubernetes manifests
-kompose convert -f ../docker-compose.yml
-
 # Loop through each generated Kubernetes manifest file
 for file in *.yaml; do
     # Set NodePort for UI service
