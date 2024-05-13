88224203bc9afdffbaa54d89323903e6d9b6ecf6
Ziya Asici <ziya.asici@yahoo.com>
2024-05-13 16:27:24 +0000

    Adding Solutions

--- Solution-Files/Task4-Hoca/Terraform/main.tf
+++ Solution-Files/Task4-Hoca/Terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "ec2-sec-grp" {
-  name        = "Task44-Sec-Grp"
-  description = "Task44 Security Group"
+  name        = "Task4-Sec-Grp"
+  description = "Tas44 Security Group"
   tags = {
-    Name = "Task44-Sec-Grp"
+    Name = "Task4-Sec-Grp"
   }
 
   dynamic "ingress" {
