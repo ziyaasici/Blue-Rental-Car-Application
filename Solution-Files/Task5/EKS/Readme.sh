f0bb87dc1c489daeca7d648a019393a8f1155e4f
Ziya Asici <ziya.asici@yahoo.com>
2024-05-14 20:00:08 +0000

    Helm Test

--- Solution-Files/Task5/EKS/Readme.sh
+++ Solution-Files/Task5/EKS/Readme.sh
@@ -5,9 +5,6 @@ chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
-sudo yum install git -y
-git clone #XXXXXXXXXXXXXX
-
 aws configure #YAPILACAK JENKINS MAKINADA GEREK OLMAYABILIR ADMIN YETKISI OLDUGU ICIN
 
 
@@ -22,8 +19,24 @@ aws eks --region "us-east-1" update-kubeconfig --name "Blue-Car-Rental-EKS"
 aws eks create-nodegroup --cli-input-json file://nodegroup-config.json
 aws eks describe-nodegroup --cluster-name "Blue-Car-Rental-EKS" --nodegroup-name "Blue-Car-Rental-NodeGroup" --query "nodegroup.status" # ACTIVE OLMASINI BEKLE
 
+sudo yum install git -y
+git clone https://ziyaasici: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@github.com/ziyaasici/Blue-Rental-Car-Application.git
 
+#Install HELM
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+#S3 Olustur
+aws s3 mb s3://ziyaasici-task5-blue-rental-car --region us-east-1
+echo "" | aws s3 cp - s3://ziyaasici-task5-blue-rental-car/blue-rental-car/dummyfile
+
+#Install Helm Plugin
+helm plugin install https://github.com/hypnoglow/helm-s3.git
+
+# S3 Init
+helm s3 init s3://ziyaasici-task5-blue-rental-car/blue-rental-car/
+
+# Helm Repo Ekle
+helm repo add stable-myapp s3://ziyaasici-task5-blue-rental-car/blue-rental-car/
+
+# Helm
+helm package ./Solution-Files/Task5/k8s
