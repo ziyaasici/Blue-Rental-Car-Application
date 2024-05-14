1b163e9825711541eb08ed800011c6adaac4db73
Ziya Asici <ziya.asici@yahoo.com>
2024-05-14 02:03:38 +0000

    Task5 Initial

--- Solution-Files/Task5/Terraform/eks.tf
+++ Solution-Files/Task5/Terraform/eks.tf
@@ -11,3 +11,24 @@ resource "aws_eks_cluster" "EKS" {
     aws_iam_role_policy_attachment.eks_service_policy
   ]
 }
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.EKS.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  instance_types = ["t3a.medium"]
+
+  ami_type = "AL2_x86_64"
+
+  depends_on = [
+    aws_eks_cluster.my_cluster
+  ]
+}
--- Solution-Files/Task5/Terraform/vpc.tf
+++ Solution-Files/Task5/Terraform/vpc.tf
@@ -5,9 +5,11 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.eks_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id     = aws_vpc.eks_vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
